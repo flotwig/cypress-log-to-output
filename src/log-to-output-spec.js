774cd17f65223b31c8e67732287f75a379a260a5
Adam Murray <adamm@cypress.io>
2022-07-26 13:35:59 -0500

    feat: update plugin to be compatible with Cypress 10. BREAKING CHANGE: updates config for Cypress 10 and updates plugin to use es6 import/exports.

--- src/log-to-output-spec.js
+++ src/log-to-output-spec.js
@@ -1,37 +1,28 @@
-const assert = require('assert')
-const logToOutput = require('./log-to-output')
+import * as logToOutput from './log-to-output.js';
+import assert from '';
 
 describe('log-to-output', function () {
   context('ensureRdpPort', function () {
     it('returns the existing port if there is one', function () {
-      const args = [
-        '--foo',
-        '--bar',
-        '--remote-debugging-port=12345',
-        '--baz'
-      ]
+      const args = ['--foo', '--bar', '--remote-debugging-port=12345', '--baz'];
 
-      const argsCopy = args.slice()
+      const argsCopy = args.slice();
 
-      const actual = logToOutput._ensureRdpPort(args)
+      const actual = logToOutput._ensureRdpPort(args);
 
-      assert(actual === 12345)
-      assert(args.length === argsCopy.length)
-    })
+      assert(actual === 12345);
+      assert(args.length === argsCopy.length);
+    });
 
     it('adds a new arg and returns new port if there is not one', function () {
-      const args = [
-        '--foo',
-        '--bar',
-        '--baz'
-      ]
+      const args = ['--foo', '--bar', '--baz'];
 
-      const argsCopy = args.slice()
+      const argsCopy = args.slice();
 
-      const actual = logToOutput._ensureRdpPort(args)
+      const actual = logToOutput._ensureRdpPort(args);
 
-      assert(args.length === argsCopy.length + 1)
-      assert(args[args.length - 1] === `--remote-debugging-port=${actual}`)
-    })
-  })
-})
+      assert(args.length === argsCopy.length + 1);
+      assert(args[args.length - 1] === `--remote-debugging-port=${actual}`);
+    });
+  });
+});
